8bee7f3ded1b4f94bd8de8ec8a0e3575b07b619d
Zayd <zayddhcf@gmail.com>
2017-09-09 00:38:11 +1000

    Fixed a heap of shit.
    for meeran ^_^

--- src/main/java/me/meeran/staffjoinsound/Core.java
+++ src/main/java/me/meeran/staffjoinsound/Core.java
@@ -4,28 +4,24 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-
-
 public class Core extends JavaPlugin {
 
-    public void onEnable() {
+    public void onEnable() 
+    {
+        Bukkit.getConsoleSender.sendMessage("StaffSound by Meeran has been enabled, you can view this public repository at https://github.com/FreeMeeranCoding/StaffJoinSound");
         this.getConfig();
         getConfig().addDefault("config", true);
-        this.Commands();
-        this.Listener();
+        this.registerCommands();
+        this.registerListeners();
     }
-
-
-    public void Commands() {
+    
+    public void registerCommands() 
+    {
         this.getCommand("staffsound").setExecutor(new StaffSoundCommand());
-
     }
 
-    public void Listener() {
+    public void registerListeners() 
+    {
         this.getServer().getPluginManager().registerEvents((Listener) new Listeners(), (Plugin) this);
     }
-
-
-
-
 }
